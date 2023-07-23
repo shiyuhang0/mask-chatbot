9f331f42703c1a45ece8dbd19a9f2a43e72065a4
shiyuhang <1136742008@qq.com>
2023-07-23 16:48:12 +0800

    add prompt

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -4,8 +4,17 @@ import { Button } from '@/components/ui/button'
 import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
 
-import {SelectPrompt} from '@/components/prompt-select';
 import * as React from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
+import {GetPrompts} from "@/app/prompts";
 
 const exampleMessages = [
   {
@@ -22,44 +31,59 @@ const exampleMessages = [
   }
 ]
 
-export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
+export async function EmptyScreen({setInput}: Pick<UseChatHelpers, 'setInput'>) {
+  const prompt = await GetPrompts()
   return (
-    <div className="mx-auto max-w-2xl px-4">
-      <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
-        </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
-        </p>
-        <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
-        </p>
-        <div className="mt-4 flex flex-col items-start space-y-2">
-          {exampleMessages.map((message, index) => (
-            <Button
-              key={index}
-              variant="link"
-              className="h-auto p-0 text-base"
-              onClick={() => setInput(message.message)}
-            >
-              <IconArrowRight className="mr-2 text-muted-foreground" />
-              {message.heading}
-            </Button>
-          ))}
-        </div>
-        <div>
-          <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
-            {/* @ts-ignore */}
-            <SelectPrompt />
-          </React.Suspense>
+      <div className="mx-auto max-w-2xl px-4">
+        <div className="rounded-lg border bg-background p-8">
+          <h1 className="mb-2 text-lg font-semibold">
+            Welcome to Next.js AI Chatbot!
+          </h1>
+          <p className="mb-2 leading-normal text-muted-foreground">
+            This is an open source AI chatbot app template built with{' '}
+            <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
+            <ExternalLink href="https://vercel.com/storage/kv">
+              Vercel KV
+            </ExternalLink>
+            .
+          </p>
+          <p className="leading-normal text-muted-foreground">
+            You can start a conversation here or try the following examples:
+          </p>
+          <div className="mt-4 flex flex-col items-start space-y-2">
+            {exampleMessages.map((message, index) => (
+                <Button
+                    key={index}
+                    variant="link"
+                    className="h-auto p-0 text-base"
+                    onClick={() => setInput(message.message)}
+                >
+                  <IconArrowRight className="mr-2 text-muted-foreground"/>
+                  {message.heading}
+                </Button>
+            ))}
+          </div>
+          <div>
+            <React.Suspense fallback={<div className="flex-1 overflow-auto"/>}>
+              {/* @ts-ignore */}
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Act as"/>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Act as</SelectLabel>
+                    {prompt.map((item, index) => (
+                        <SelectItem key={index} value={item.act}>
+                          {item.act}
+                        </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </React.Suspense>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
