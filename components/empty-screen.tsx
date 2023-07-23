1bc4b60d78723f88a8794a06295841ba31d11d2d
shiyuhang <1136742008@qq.com>
2023-07-23 17:36:52 +0800

    add prompt

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -49,22 +49,22 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
         </p>
-        <div className="mt-4 flex flex-col items-start space-y-2">
-          {exampleMessages.map((message, index) => (
-            <Button
-              key={index}
-              variant="link"
-              className="h-auto p-0 text-base"
-              onClick={() => setInput(message.message)}
-            >
-              <IconArrowRight className="mr-2 text-muted-foreground" />
-              {message.heading}
-            </Button>
-          ))}
-        </div>
+        {/*<div className="mt-4 flex flex-col items-start space-y-2">*/}
+        {/*  {exampleMessages.map((message, index) => (*/}
+        {/*    <Button*/}
+        {/*      key={index}*/}
+        {/*      variant="link"*/}
+        {/*      className="h-auto p-0 text-base"*/}
+        {/*      onClick={() => setInput(message.message)}*/}
+        {/*    >*/}
+        {/*      <IconArrowRight className="mr-2 text-muted-foreground" />*/}
+        {/*      {message.heading}*/}
+        {/*    </Button>*/}
+        {/*  ))}*/}
+        {/*</div>*/}
         <div>
           <Select onValueChange={(value)=>setInput(value)}>
-            <SelectTrigger className="w-[180px]">
+            <SelectTrigger className="w-[300px]">
               <SelectValue placeholder="Act as"/>
             </SelectTrigger>
             <SelectContent>
