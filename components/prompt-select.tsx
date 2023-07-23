e3cebe19b4595a5f2e97992dade2af326eaed4e1
shiyuhang <1136742008@qq.com>
2023-07-23 16:21:10 +0800

    add prompt

--- components/prompt-select.tsx
+++ components/prompt-select.tsx
@@ -11,20 +11,20 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
-export function SelectPrompt() {
+export async function SelectPrompt() {
   const prompt = await GetPrompts()
   return (
       <Select>
         <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Act as" />
+          <SelectValue placeholder="Act as"/>
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
             <SelectLabel>Act as</SelectLabel>
             {prompt.map((item, index) => (
-              <SelectItem key={index} value={item.act}>
-                {item.act}
-              </SelectItem>
+                <SelectItem key={index} value={item.act}>
+                  {item.act}
+                </SelectItem>
             ))}
           </SelectGroup>
         </SelectContent>
