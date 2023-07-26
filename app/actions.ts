9bb5b72b70cfe72605ad0489585624947639f70c
shiyuhang <1136742008@qq.com>
2023-07-26 11:47:33 +0800

    te3

--- app/actions.ts
+++ app/actions.ts
@@ -11,9 +11,7 @@ const useKV = process.env.USE_KV === 'true'
 
 export async function GetPrompts() {
   console.log('get prompts')
-  const res = await fetch(`https://${process.env.VERCEL_URL}/api/prompts`,{
-
-  })
+  const res = await fetch(`https://${process.env.VERCEL_URL}/api/prompts`,{ cache: 'no-cache' })
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
