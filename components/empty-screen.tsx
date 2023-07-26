eba2add139fa49d841bba63325c11fad09e273f3
Leon Yang <yangxin@pingcap.com>
2023-07-26 17:11:54 +0800

    Add scroll area (#10)

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,4 +1,4 @@
-import { UseChatHelpers } from 'ai/react'
+import {UseChatHelpers} from 'ai/react'
 
 import * as React from 'react'
 import {
@@ -10,6 +10,18 @@ import {
   SelectTrigger,
   SelectValue
 } from "@/components/ui/select";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import { ScrollArea } from "@/components/ui/scroll-area"
+import {Button} from "@/components/ui/button";
+import {Check, ChevronsUpDown} from "lucide-react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem
+} from "@/components/ui/command";
+import {cn} from "@/lib/utils";
 import {InsertPrompt} from "@/components/prompt-insert";
 
 const exampleMessages = [
@@ -46,44 +58,74 @@ type Prompts = {
   prompt: string
 }
 
-export function EmptyScreen({ setInput,prompts }: EmptyScreenProps) {
+export function EmptyScreen({setInput, prompts}: EmptyScreenProps) {
   const rows: Rows = JSON.parse(prompts)
-  const myPrompts : Prompts[] = rows.rows
+  const myPrompts: Prompts[] = rows.rows
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
   return (
-    <div className="mx-auto max-w-2xl px-4">
-      <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to AI Chatbot!
-        </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app with awesome prompts, helping you be a better prompt engineer.
-        </p>
-        <p className="leading-normal text-muted-foreground">
-          You can select a role and we will generate the best prompt for you:
-        </p>
-        <div className="mt-4 flex flex-row items-start space-x-1">
-          <div className="items-start">
-            <Select onValueChange={(value) => setInput(value)}>
-              <SelectTrigger className="w-[300px]">
-                <SelectValue placeholder="Act as"/>
-              </SelectTrigger>
-              <SelectContent hideWhenDetached={true}>
-                <SelectGroup>
-                  <SelectLabel>Act as</SelectLabel>
-                  {myPrompts.map((item, index) => (
-                      <SelectItem key={index} value={item.prompt}>
-                        {item.act}
-                      </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="items-center self-center space-x-6 pl-4">
-            <InsertPrompt />
+      <div className="mx-auto max-w-2xl px-4">
+        <div className="rounded-lg border bg-background p-8">
+          <h1 className="mb-2 text-lg font-semibold">
+            Welcome to AI Chatbot!
+          </h1>
+          <p className="mb-2 leading-normal text-muted-foreground">
+            This is an open source AI chatbot app with awesome prompts, helping you be a better
+            prompt engineer.
+          </p>
+          <p className="leading-normal text-muted-foreground">
+            You can select a role and we will generate the best prompt for you:
+          </p>
+          <div className="mt-4 flex flex-col items-start space-y-2">
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-fit justify-between"
+                >
+                  {value
+                      ? value
+                      : "Act as..."}
+                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50"/>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[250px] p-0">
+                <Command className="w-[250px]">
+                  <CommandInput placeholder="Search ..."/>
+                  <CommandEmpty>No framework found.</CommandEmpty>
+                  <ScrollArea className='h-screen max-h-[60vh] overflow-auto'>
+                    <div className="items-center self-center space-x-6 pl-4">
+                      <InsertPrompt />
+                    </div>
+                    <CommandGroup>
+                      {myPrompts.map((prompt) => (
+                          <CommandItem
+                              key={prompt.act}
+                              onSelect={(currentValue) => {
+                                setValue(currentValue)
+                                setInput(prompt.prompt)
+                                setOpen(false)
+                              }}
+                              value={prompt.act}
+                          >
+                            <Check
+                                className={cn(
+                                    "mr-2 h-4 w-4",
+                                    value === prompt.act ? "opacity-100" : "opacity-0"
+                                )}
+                            />
+                            {prompt.act}
+                          </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </ScrollArea>
+                </Command>
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
       </div>
-    </div>
   )
 }
