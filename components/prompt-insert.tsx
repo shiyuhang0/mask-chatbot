8ba3bedf989fb4bfdd2f0c32023b4215f6b759b7
shiyuhang <1136742008@qq.com>
2023-07-27 15:00:08 +0800

    make add prompt isloate

--- components/prompt-insert.tsx
+++ components/prompt-insert.tsx
@@ -17,7 +17,7 @@ import {AddPrompt} from "@/app/actions";
 import {useRouter} from "next/navigation";
 import {Textarea} from "@/components/ui/textarea";
 
-export function InsertPrompt(userId: string) {
+export function InsertPrompt({userId}: string) {
   const [act, setAct] = useState('');
   const [prompt, setPrompt] = useState('');
   const router = useRouter()
