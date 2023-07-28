630be31bbadf500755ae16c4903ec1bd51a2423f
shiyuhang <1136742008@qq.com>
2023-07-28 10:34:28 +0800

    opt etxt

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -49,11 +49,10 @@ export function EmptyScreen({setInput, prompts}: EmptyScreenProps) {
       <div className="mx-auto max-w-2xl px-4">
         <div className="rounded-lg border bg-background p-8">
           <h1 className="mb-2 text-lg font-semibold">
-            Be a prompt engineer!
+            Mask AI Chatbot!
           </h1>
           <p className="mb-2 leading-normal text-muted-foreground">
-            This AI chatbot app helps you become a qualified prompt engineer.
-            Select a role to generate the most suitable prompt:
+            The Mask AI chatbot helps you generate the most suitable prompt, makes you a junior prompt engineer.
           </p>
           {/*<p className="leading-normal text-muted-foreground">*/}
           {/*  Select a role and we will generate the best prompt for you:*/}
